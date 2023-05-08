c5f83455f7c9fd54b9bfa7f6e68f4ea733094638
aman2212002 <amanrajawat2212002@gmail.com>
2023-05-08 23:28:05 +0530

    day4

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -3,20 +3,12 @@ import Split from 'react-split'
 
 function Login(){
     return(
-      <div>
-         <Split
-         sizes={[25, 75]}
-         minSize={100}
-         
-         gutterSize={3}
-         gutterAlign="center"
-         
-         direction="horizontal"
-         cursor="col-resize">
+      
+         <Split className="split">
        <div className="leftside">hello</div>
        <div className="rightside">world</div>
     </Split>
-      </div> 
+     
        
     );
 }
